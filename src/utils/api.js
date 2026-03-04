54039faf87acae388b16ea6202215244164d9ca2
PoojiKatru <116402849+PoojiKatru@users.noreply.github.com>
2026-03-03 19:32:20 -0800

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,19 +12,4 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-api.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    if (err.response?.status === 401 && window.location.pathname !== '/login') {
-      const msg = err.response?.data?.detail || '';
-      if (msg === 'Invalid token' || msg === 'User not found') {
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-        window.location.href = '/login';
-      }
-    }
-    return Promise.reject(err);
-  }
-);
-
 export default api;
